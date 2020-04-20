61b128b4c2deccdc68b5100fb6ffce9db6ebc84f
Hamish Macpherson <hamstu@gmail.com>
2020-04-20 15:50:59 -0700

    implement random post text

--- cypress/integration/first.js
+++ cypress/integration/first.js
@@ -1,11 +1,16 @@
+const getRandomPostText = () => {
+  return `This is my random post text - ${Math.random()
+    .toString(36)
+    .substring(7)}`;
+};
+
 describe('App', function() {
   it('creates a new post', function() {
     cy.login();
     cy.visit('/');
     cy.get('[data-cy=open-composer-button]').click();
-    cy.get('[data-cy=composer-text-zone').type(
-      'This is my first post!{cmd}{enter}'
-    );
-    cy.get('[data-cy=post]').should('contain', 'This is my first post!');
+    const randomPostText = getRandomPostText();
+    cy.get('[data-cy=composer-text-zone').type(`${randomPostText}{cmd}{enter}`);
+    cy.get('[data-cy=post]').should('contain', randomPostText);
   });
 });
